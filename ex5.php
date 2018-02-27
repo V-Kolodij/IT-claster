656bd761a0697a992b68c7f36521300def930a7f
Volodymyr Kolodij <kolodijv80@gmail.com>
2018-02-27 14:31:43 +0200

    Zadachi

---
+++ ex5.php
@@ -0,0 +1,18 @@
+﻿<?php
+$name = $_POST["name"];
+$litr = (int)$_POST["milk"]; //к-ть проданого молока в літрах
+$vartist = 6; //вартість 1 літра молока
+if (0 < $litr && $litr < 1000){
+	$zp = $litr*$vartist*0.2;
+	echo "<p>Зарплатня $name становить $zp гривень";
+}elseif (1000 <= $litr && $litr <= 2000) {
+	$zp = $litr*$vartist*0.25;
+	echo "<p>Зарплатня $name становить $zp гривень";
+}elseif ($litr > 2000) {
+	$zp = $litr*$vartist*0.3;
+	echo "Зарплатня $name становить $zp гривень";
+}else{
+	echo "<p>$name не продав жодного літра, і зарплату не отримає.";
+}
+
+?>
---
+++ ex7.php
@@ -0,0 +1,29 @@
+﻿<?php
+$num = (int)$_POST["day"];
+
+switch ($num) {
+	case 1:
+		echo "Це Понеділок";
+		break;
+	case 2:
+		echo "Це Вівторок";
+		break;
+	case 3:
+		echo "Це Середа";
+		break;
+	case 4:
+		echo "Це Четвер";
+		break;
+	case 5:
+		echo "Це П'ятниця";
+		break;
+	case 6:
+		echo "Це Субота";
+		break;
+	case 7:
+		echo "Це Неділя";
+		break;
+}
+
+
+?>
